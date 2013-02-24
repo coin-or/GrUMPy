236cf3df5ddafa278d60ca63c7be3e543c9c5b70
tkr <tkr@5eb3cf9a-c56f-4155-9a08-806ae8551a6d>
2013-02-24 23:46:51 +0000

    Adding branch and bound to grumpy
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/GrUMPy/trunk@775 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- grumpy.py
+++ grumpy.py
@@ -88,13 +88,24 @@ import subprocess
 import sys, os
 from subprocess import Popen, PIPE, STDOUT
 import optparse
-from gimpy import BinaryTree
+from gimpy import BinaryTree, Cluster, etree_installed, pygame_installed, xdot_installed
 from gimpy import quote_if_necessary as quote
 import time
+from Queues import PriorityQueue
 
 from StringIO import StringIO
 from pygame import display, image, init, Rect
 from pygame.transform import scale
+from pulp import LpVariable, lpSum, LpProblem, LpMaximize, LpConstraint, LpStatus, value
+
+try:
+    from gexf import Gexf
+except ImportError:
+    gexf_installed = False
+    print 'Gexf not installed'
+else:
+    gexf_installed = True
+    print 'Found gexf installation'
 
 from forecastingchainedsequences import ForecastingChainedSequences
 
@@ -170,7 +181,6 @@ class BBTree(BinaryTree):
             return
         if mode == 'Dot':
             try:
-                
                 gexf = Gexf("Mike O'Sullivan", "Dynamic graph file")
                 graph = gexf.addGraph("directed", "dynamic", "Dynamic graph")
                 objAtt = graph.addNodeAttribute("obj", "0.0", "float")
@@ -1865,7 +1875,6 @@ class BBTree(BinaryTree):
         Path can be a string or a file handle.
         Attempt to uncompress/compress files ending in '.gz' and '.bz2'.
         '''
-        import os
         if self._is_string_like(path):
             if path.endswith('.gz'):
 #               import gzip
@@ -1884,13 +1893,360 @@ class BBTree(BinaryTree):
             raise TypeError('path must be a string or file handle.')
         return fh
     
-    def _is_string_like(self,obj): # from John Hunter, types-free version
+    def _is_string_like(self, obj): # from John Hunter, types-free version
         try:
             obj + ''
         except (TypeError, ValueError):
             return False
         return True
 
+    def GenerateRandomMIP(self, numVars = 40, numCons = 20, density = 0.2, 
+                            maxObjCoeff = 10, maxConsCoeff = 10, rand_seed = 2):
+        random.seed(rand_seed)
+        CONSTRAINTS = ["C"+str(i) for i in range(numCons)]
+        if self.get_layout() == 'dot2tex':
+            VARIABLES = ["x_{"+str(i)+"}" for i in range(numVars)]
+        else:
+            VARIABLES = ["x"+str(i) for i in range(numVars)]
+        OBJ = {i : random.randint(1, maxObjCoeff) for i in VARIABLES}
+        MAT = {i : [random.randint(1, maxConsCoeff) if random.random() <= density else 0
+                    for j in CONSTRAINTS] for i in VARIABLES}
+        RHS = [random.randint(int(numVars*density*maxConsCoeff/2), 
+                       int(numVars*density*maxConsCoeff/1.5)) 
+               for i in CONSTRAINTS]
+
+        return CONSTRAINTS, VARIABLES, OBJ, MAT, RHS
+    
+    def BranchAndBound(self, CONSTRAINTS, VARIABLES, OBJ, MAT, RHS,
+                       branch_strategy = 'Most Fractional',
+                       search_strategy = 'Depth First',
+                       complete_enumeration = False,
+                       display_interval = None):
+        
+        #Add key
+        #C = Cluster(graph_name = 'Key', label = 'Key', fontsize = '12')
+        #C.add_node('C', label = 'Candidate', style = 'filled', color = 'yellow', fillcolor = 'yellow')
+        #C.add_node('I', label = 'Infeasible', style = 'filled', color = 'orange', fillcolor = 'orange')
+        #C.add_node('S', label = 'Solution', style = 'filled', color = 'lightblue', fillcolor = 'lightblue')
+        #C.add_node('P', label = 'Pruned', style = 'filled', color = 'red', fillcolor = 'red')
+        #C.add_edge('C', 'I', style = 'invisible', arrowhead = 'none')
+        #C.add_edge('I', 'S', style = 'invisible', arrowhead = 'none')
+        #C.add_edge('S', 'P', style = 'invisible', arrowhead = 'none')
+        #self.add_subgraph(C)
+
+        INFINITY = 9999
+        #The initial lower bound 
+        LB = -INFINITY
+
+        #The number of LP's solved, and the number of nodes solved
+        node_count = 1
+        iter_count = 0
+        lp_count = 0
+
+        var   = LpVariable.dicts("", VARIABLES, 0, 1)
+        numCons = len(CONSTRAINTS)
+        numVars = len(VARIABLES)
+
+        #List of incumbent solution variable values
+        opt = dict([(i, 0) for i in VARIABLES]) 
+
+        print "==========================================="
+        print "Starting Branch and Bound"
+ 
+        if branch_strategy == 'Most Fraction':
+            print "Most fractional variable"
+        elif branch_strategy == 'Fixed':
+            print "Fixed order"
+        else:
+            print "Unknown branching strategy %s" %branch_strategy
+
+            if search_strategy == 'Depth First':
+                print "Depth first search strategy"
+            elif search_strategy == 'Best First':
+                print "Best first search strategy"
+            else:
+                print "Unknown search strategy %s" %search_strategy
+
+            print "==========================================="
+
+        # List of candidate nodes
+        Q = PriorityQueue()
+
+        # The current tree depth
+        cur_depth = 0
+        cur_index = 0
+
+        # Timer
+        timer = time.time()
+
+        Q.push((0, None, None, None, None), INFINITY)
+
+        # Branch and Bound Loop
+        while not Q.isEmpty():
+
+            cur_index, parent, branch_var, sense, rhs = Q.pop()
+            if cur_index is not 0:
+                cur_depth = self.get_node_attr(parent, 'level') + 1
+            else:
+                cur_depth = 0
+
+            print ""
+            print "----------------------------------------------------"
+            print ""
+            print "Node: %s, Depth: %s, LB: %s" %(cur_index,cur_depth,LB)
+
+            #====================================
+            #    LP Relaxation
+            #====================================
+            #Compute lower bound by LP relaxation
+            prob = LpProblem("relax", LpMaximize)
+            prob += lpSum([OBJ[i]*var[i] for i in VARIABLES]), "Objective"
+            for j in range(numCons):
+                prob += (lpSum([MAT[i][j]*var[i] for i in VARIABLES])<=RHS[j], \
+                             CONSTRAINTS[j])
+
+            #Fix all prescribed variables
+            branch_vars = []
+            if cur_index is not 0:
+                sys.stdout.write("Branching variables: ")
+                branch_vars.append(branch_var)
+                if sense == '>=':
+                    prob += LpConstraint(lpSum(var[branch_var]) >= rhs)
+                else:
+                    prob += LpConstraint(lpSum(var[branch_var]) <= rhs)   
+                print branch_var,
+                pred = parent
+                while str(pred) is not '0':
+                    pred_branch_var = self.get_node_attr(pred, 'branch_var')
+                    pred_rhs = self.get_node_attr(pred, 'rhs')
+                    pred_sense = self.get_node_attr(pred, 'sense')
+                    if pred_sense == '<=':
+                        prob += LpConstraint(lpSum(var[pred_branch_var]) 
+                                             <= pred_rhs)
+                    else:
+                        prob += LpConstraint(lpSum(var[pred_branch_var]) 
+                                                 >= pred_rhs)
+                    print pred_branch_var,
+                    branch_vars.append(pred_branch_var)
+                    pred = self.get_node_attr(pred, 'parent')
+        
+                print ""
+
+            # Solve the LP relaxation    
+            prob.solve()
+    
+            lp_count = lp_count +1
+    
+            # Check infeasibility
+            infeasible = LpStatus[prob.status] == "Infeasible"
+ 
+            # Print status
+            if infeasible:
+                print "LP Solved, status: Infeasible"
+            else:
+                print "LP Solved, status: %s, obj: %s" %(LpStatus[prob.status],
+                                                         value(prob.objective))
+    
+ 
+            if(LpStatus[prob.status] == "Optimal"):
+                relax = value(prob.objective)
+            else:
+                relax = INFINITY
+        
+            integer_solution = 0
+    
+            if (LpStatus[prob.status] == "Optimal"):        
+                var_values = dict([(i, var[i].varValue) for i in VARIABLES])
+                integer_solution = 1
+                for i in VARIABLES:
+                    if (var_values[i] not in set([0,1])):
+                        integer_solution = 0
+                        break
+                if (integer_solution and relax>LB):
+                    LB = relax
+                    for i in VARIABLES:
+                        #these two have different data structures first one 
+                        #list, second one dictionary
+                        opt[i] = var_values[i] 
+                    print "New best solution found, objective: %s" %relax 
+                    for i in VARIABLES:
+                        if var_values[i] > 0:
+                            print "%s = %s" %(i, var_values[i])
+                elif (integer_solution and relax<=LB):
+                    print "New integer solution found, objective: %s" %relax 
+                    for i in VARIABLES:
+                        if var_values[i] > 0:
+                            print "%s = %s" %(i, var_values[i])
+                else:
+                    print "Fractional solution:"
+                    for i in VARIABLES:
+                        if var_values[i] > 0:
+                            print "%s = %s" %(i, var_values[i])
+
+                #For complete enumeration
+                if complete_enumeration:
+                    relax = LB - 1
+      
+            if integer_solution:
+                print "Integer solution"
+                status = 'S'
+                BAKstatus = 'integer'
+                color = 'lightblue'
+            elif infeasible:
+                print "Infeasible node"
+                status = 'I'
+                BAKstatus = 'infeasible'
+                color = 'orange'
+            elif not complete_enumeration and relax <= LB:
+                print "Node pruned by bound (obj: %s, UB: %s)" %(relax,LB)
+                status = 'P'
+                BAKstatus = 'fathomed'
+                color = 'red'
+            elif cur_depth >= numVars :
+                print "Reached a leaf"
+                BAKstatus = 'fathomed'
+                status = 'L'
+            else:
+                status = 'C'
+                BAKstatus = 'candidate'
+                color = 'yellow'
+
+            if status is not 'I':
+#               label = status + ": " + "%.1f"%relax
+                label = "%.1f"%relax
+            else:
+                label = 'I'
+
+            if iter_count == 0:
+                if  self.get_layout() == 'bak':
+                    self.AddOrUpdateNode(0, -1, None, BAKstatus, -relax, None, 
+                                      None)
+                else:
+                    self.add_root(0, label = label, status = status, obj = relax, 
+                               color = color, style = 'filled', 
+                               fillcolor = color)
+                if etree_installed and self.display_mode == 'svg':
+                    self.write_as_svg(filename = "node%d" % iter_count, 
+                                   nextfile = "node%d" % (iter_count + 1), 
+                                   highlight = cur_index)
+            else:
+                if  self.get_layout() == 'bak':
+                    if sense == '<=':
+                        self.AddOrUpdateNode(cur_index, parent, 'L', 'candidate', 
+                                          -relax, None, None, 
+                                          branch_var = branch_var,
+                                          sense = sense, rhs = rhs)
+                    else:
+                        self.AddOrUpdateNode(cur_index, parent, 'R', 'candidate', 
+                                          -relax, None, None, 
+                                          branch_var = branch_var,
+                                          sense = sense, rhs = rhs)
+                else:
+                    self.add_child(cur_index, parent, label = label, 
+                                branch_var = branch_var,
+                                sense = sense, rhs = rhs, status = status, 
+                                obj = relax, color = color, style = 'filled', 
+                                fillcolor = color)
+                    if  self.get_layout() == 'dot2tex':
+                        if sense == '>=':
+                            self.set_edge_attr(parent, cur_index, 'label', 
+                                               str(branch_var) + " \geq " + 
+                                               str(rhs))
+                        else:
+                            self.set_edge_attr(parent, cur_index, 'label', 
+                                               str(branch_var) + " \leq " + 
+                                               str(rhs))
+                    else:
+                        self.set_edge_attr(parent, cur_index, 'label', 
+                                        str(branch_var) + sense + str(rhs))
+                if etree_installed and self.display_mode == 'svg':
+                    self.write_as_svg(filename = "node%d" % iter_count, 
+                                   prevfile = "node%d" % (iter_count - 1), 
+                                   nextfile = "node%d" % (iter_count + 1), 
+                                   highlight = cur_index)
+            iter_count += 1
+
+            if ((pygame_installed and self.display_mode == 'pygame')
+                or (xdot_installed and self.display_mode == 'xdot')):
+                numNodes = len(self.get_node_list())
+                if display_interval is not None and numNodes % display_interval == 0 and not self.get_layout() != 'dot2tex':
+                    self.display(highlight = [cur_index])
+            elif gexf_installed and self.display_mode == 'gexf':
+                self.write_as_dynamic_gexf("graph")
+
+            if status == 'C':
+  
+                # Branching:
+                # Choose a variable for branching
+                branching_var = -1
+                if branch_strategy == 'Fixed':
+                    #fixed order
+                    for i in VARIABLES:
+                        frac = min(var[i].varValue-math.floor(var[i].varValue), 
+                                   math.ceil(var[i].varValue) - var[i].varValue)
+                        if (frac > 0):
+                            min_frac = frac
+                            branching_var = i
+                            break
+                elif branch_strategy == 'Most Fractional':
+                    #most fractional variable
+                    min_frac = -1
+                    for i in VARIABLES:
+                        frac = min(var[i].varValue-math.floor(var[i].varValue), 
+                                   math.ceil(var[i].varValue)- var[i].varValue )
+                        if (frac> min_frac):
+                            min_frac = frac
+                            branching_var = i
+
+                else:
+                    print "Unknown branching strategy %s" %branch_strategy
+                    exit()
+    
+                if branching_var >= 0:
+                    print "Branching on variable %s" %branching_var
+            
+                #Create new nodes
+                if search_strategy == 'Depth First':
+                    priority = -cur_depth - 1
+                elif search_strategy == 'Best First':
+                    priority = relax
+                node_count += 1
+                Q.push((node_count, cur_index, branching_var, '<=', 
+                        math.floor(var[branching_var].varValue)), priority)
+                node_count += 1
+                Q.push((node_count, cur_index, branching_var, '>=', 
+                        math.ceil(var[branching_var].varValue)), priority)
+                self.set_node_attr(cur_index, color, 'green')
+                if  self.get_layout() == 'bak':
+                    self.set_node_attr(cur_index, 'status', 'branched')
+ 
+        timer = int(math.ceil((time.time()-timer)*1000))
+
+        if ((xdot_installed and self.display_mode == 'xdot' and  self.get_layout() != 'dot2tex') or
+             self.get_layout() == 'bak'):
+            self.display()
+#            if  self.get_layout() == 'dot2tex':
+#                print self.write_as_dot(filename = 'graph')
+    
+        print ""
+        print "==========================================="
+        print "Branch and bound completed in %sms" %timer
+        print "Strategy: %s" %branch_strategy
+        if complete_enumeration:
+            print "Complete enumeration"
+        print "%s nodes visited " %node_count
+        print "%s LP's solved" %lp_count     
+        print "==========================================="
+        print "Optimal solution"
+        #print optimal solution
+        for i in sorted(VARIABLES):
+            if opt[i] > 0:
+                print "%s = %s" %(i, opt[i])
+        print "Objective function value"
+        print LB          
+        print "==========================================="    
+
+        return opt, LB 
 
 def CreatePerlStyleBooleanFlag(parser, flag_text, variable_name, help_text):
     """Add two options to an optparse.OptionParser, one with a 'no' prefix.
@@ -2021,3 +2377,15 @@ def parse_options():
         options.scatterplot = True
 
     return (input_filename, options)
+
+    
+if __name__ == '__main__':
+    
+    T = BBTree()
+    T.set_layout('dot2tex')
+    T.set_display_mode('file')    
+#    T.set_layout('dot')
+#    T.set_display_mode('xdot')
+    CONSTRAINTS, VARIABLES, OBJ, MAT, RHS = T.GenerateRandomMIP()
+    T.BranchAndBound(CONSTRAINTS, VARIABLES, OBJ, MAT, RHS)
+    T.display(basename='graph', format='pdf')
