52a3f6eb63684fdb10897fcb1f5e236ef0adf063
Aykut Bulut <aykut@lehigh.edu>
2013-05-06 19:40:42 +0000

    test/test_display.py is updated. It includes examples from all possible display modes.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/GrUMPy/branches/reimplementation@919 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- test/test_display.py
+++ test/test_display.py
@@ -7,13 +7,20 @@ you close the gtk window. Gtk window will pop up again when the required number
 of nodes (display_interval many) is added to the tree.
 2. If you want to get only the final tree give sys.maxiter as input.
 
-How to use 
+How to use
 '''
 from grumpy import BBTree
 import sys
 
 if __name__=='__main__':
-    bt = BBTree(display='xdot')
+    #======== file display mode, no layout specified, save in png format
+    #bt = BBTree(display='file')
+    #======== xdot display mode
+    #bt = BBTree(display='xdot')
+    #======== pygame display mode, no layout specified, save in png format
+    #bt = BBTree(display='pygame')
+    #======== file display mode, with dot2tex layout
+    bt = BBTree(display='file', layout = 'dot2tex')
     CONSTRAINTS, VARIABLES, OBJ, MAT, RHS = bt.GenerateRandomMIP(numVars=30, numCons=10, rand_seed = 0)
     bt.BranchAndBound(CONSTRAINTS, VARIABLES, OBJ, MAT, RHS,
                      branch_strategy = 'Pseudocost', search_strategy = 'Best First',
