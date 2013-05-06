21ede77be6a3b4e8248bed6b16ba204317116979
Aykut Bulut <aykut@lehigh.edu>
2013-05-06 19:32:47 +0000

    file display mode with dot2tex layout fixed.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/GrUMPy/branches/reimplementation@918 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- grumpy/grumpy.py
+++ grumpy/grumpy.py
@@ -136,6 +136,54 @@ else:
     ETREE_INSTALLED = True
     print 'Found etree in lxml'
 
+DOT2TEX_TEMPLATE = r'''
+\documentclass[landscape]{article}
+\usepackage[x11names, rgb]{xcolor}
+\usepackage[<<textencoding>>]{inputenc}
+\usepackage{tikz}
+\usetikzlibrary{snakes,arrows,shapes}
+\usepackage{amsmath}
+\usepackage[margin=2cm,nohead]{geometry}%
+<<startpreprocsection>>%
+\usepackage[active,auctex]{preview}
+<<endpreprocsection>>%
+<<gvcols>>%
+<<cropcode>>%
+<<docpreamble>>%
+
+\begin{document}
+\pagestyle{empty}
+%
+<<startpreprocsection>>%
+<<preproccode>>
+<<endpreprocsection>>%
+%
+<<startoutputsection>>
+\enlargethispage{100cm}
+% Start of code
+% \begin{tikzpicture}[anchor=mid,>=latex',join=bevel,<<graphstyle>>]
+\resizebox{\textwidth}{!}{
+\begin{tikzpicture}[>=latex',join=bevel,<<graphstyle>>]
+\pgfsetlinewidth{1bp}
+<<figpreamble>>%
+<<drawcommands>>
+<<figpostamble>>%
+\end{tikzpicture}
+% End of code
+}
+<<endoutputsection>>
+%
+\end{document}
+%
+<<startfigonlysection>>
+\begin{tikzpicture}[>=latex,join=bevel,<<graphstyle>>]
+\pgfsetlinewidth{1bp}
+<<figpreamble>>%
+<<drawcommands>>
+<<figpostamble>>%
+\end{tikzpicture}
+<<endfigonlysection>>
+'''
 
 class BBTree(BinaryTree):
     """
