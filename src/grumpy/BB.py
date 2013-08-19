a03208b457691a2ac668c8cb8cee5012a77f345b
aykut <aykut@5eb3cf9a-c56f-4155-9a08-806ae8551a6d>
2013-08-19 19:36:29 +0000

    fixing graph titles. No time stamp in case of BB.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/GrUMPy/trunk@998 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- src/grumpy/BB.py
+++ src/grumpy/BB.py
@@ -647,8 +647,11 @@ class BBTree(BinaryTree):
         output_filename = "histogram."+index_string+".png"
         if output_file:
             script += 'set output "%s"\n' % output_filename
-        script += ('set title "Histogram of LP Bounds: %s, %s, %.2fs"\n'
-                    % (self._filename, self._label, self._time))
+        if self._filename is None:
+            script += 'set title "Histogram of LP Bounds"'
+        else:
+            script += ('set title "Histogram of LP Bounds: %s, %s, %.2fs"\n'
+                       % (self._filename, self._label, self._time))
         script += 'set xlabel "obj. value"\n'
         script += 'set ylabel "number of nodes"\n'
         if self._logscaley:
@@ -884,8 +887,11 @@ class BBTree(BinaryTree):
         # Make settings for the scatter plot.
         if output_file:
             script += 'set output "%s"\n' % output_filename
-        script += ('set title "Scatterplot: %s, %s, %ds"\n' % (
-                self._filename, self._label, int(self._time)))
+        if self._filename is None:
+            script += 'set title "Scatterplot"'
+        else:
+            script += ('set title "Scatterplot: %s, %s, %ds"\n' % (
+                    self._filename, self._label, int(self._time)))
         script += 'set pointsize 0.8\n'
         script += 'set nokey\n'
         script += 'set xlabel \"sum of int. infeas.\"\n'
@@ -974,8 +980,11 @@ class BBTree(BinaryTree):
         script = ''
         # Set terminal for the output files.
         script += 'set terminal png notransparent size 480,360\n\n'
-        script += ('set title "Incumbent path (%s %.2fs %s)"\n' % (
-                self._filename, self._time, self._label))
+        if self._filename is None:
+            script += 'set title "Incumbent path"\n'
+        else:
+            script += ('set title "Incumbent path (%s %.2fs %s)"\n' % (
+                    self._filename, self._time, self._label))
         script += 'set pointsize 0.8\n'
         script += 'set nokey\n'
         script += 'set xlabel \"sum of int. infeas.\"\n'
@@ -1089,7 +1098,10 @@ class BBTree(BinaryTree):
                                                           image_min_obj)
         data += 'set format x ""\n'
         data += 'set ylabel "obj. value"\n'
-        data += 'set title "B&B tree (%s %.2fs %s)"\n\n' % (
+        if self,_filename is None:
+            data += 'set title "B&B tree'
+        else:
+            data += 'set title "B&B tree (%s %.2fs %s)"\n\n' % (
                 self._filename, self._time, self._label)
         for line in additional_lines:
             data += line
