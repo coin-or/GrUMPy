a8b5ceab0495c8c15aea673d98ef4589b2c60c0c
Aykut Bulut <aykut@lehigh.edu>
2013-08-19 16:10:29 +0000

    test scripts updated. Results of scripts are inserted to the script documentation.
    git-svn-id: https://projects.coin-or.org/svn/CoinBazaar/projects/GIMPy/GrUMPy/trunk@996 5eb3cf9a-c56f-4155-9a08-806ae8551a6d

--- test/test_strategies.py
+++ test/test_strategies.py
@@ -1,5 +1,94 @@
 '''
-Tests various branching and search strategies.
+Tests various branching and search strategies. Compares number of LPs solved
+for different branching and searching strategies.
+
+The result of the script is as follows (until python decides to change its
+random number generator).
+
+problem     | branching strategy   | search strategy   | num lp
+---------------------------------------------------------------
+(10, 10, 0) | Most Fraction        | Depth First       | 7
+(10, 10, 0) | Most Fraction        | Best First        | 5
+(10, 10, 0) | Most Fraction        | Best Estimate     | 5
+(10, 10, 0) | Fixed Branching      | Depth First       | 7
+(10, 10, 0) | Fixed Branching      | Best First        | 5
+(10, 10, 0) | Fixed Branching      | Best Estimate     | 5
+(10, 10, 0) | Pseudocost Branching | Depth First       | 7
+(10, 10, 0) | Pseudocost Branching | Best First        | 5
+(10, 10, 0) | Pseudocost Branching | Best Estimate     | 5
+(10, 10, 1) | Most Fraction        | Depth First       | 9
+(10, 10, 1) | Most Fraction        | Best First        | 9
+(10, 10, 1) | Most Fraction        | Best Estimate     | 9
+(10, 10, 1) | Fixed Branching      | Depth First       | 9
+(10, 10, 1) | Fixed Branching      | Best First        | 9
+(10, 10, 1) | Fixed Branching      | Best Estimate     | 9
+(10, 10, 1) | Pseudocost Branching | Depth First       | 9
+(10, 10, 1) | Pseudocost Branching | Best First        | 9
+(10, 10, 1) | Pseudocost Branching | Best Estimate     | 9
+(20, 10, 2) | Most Fraction        | Depth First       | 37
+(20, 10, 2) | Most Fraction        | Best First        | 33
+(20, 10, 2) | Most Fraction        | Best Estimate     | 33
+(20, 10, 2) | Fixed Branching      | Depth First       | 35
+(20, 10, 2) | Fixed Branching      | Best First        | 31
+(20, 10, 2) | Fixed Branching      | Best Estimate     | 31
+(20, 10, 2) | Pseudocost Branching | Depth First       | 23
+(20, 10, 2) | Pseudocost Branching | Best First        | 23
+(20, 10, 2) | Pseudocost Branching | Best Estimate     | 23
+(20, 10, 3) | Most Fraction        | Depth First       | 41
+(20, 10, 3) | Most Fraction        | Best First        | 21
+(20, 10, 3) | Most Fraction        | Best Estimate     | 24
+(20, 10, 3) | Fixed Branching      | Depth First       | 57
+(20, 10, 3) | Fixed Branching      | Best First        | 39
+(20, 10, 3) | Fixed Branching      | Best Estimate     | 39
+(20, 10, 3) | Pseudocost Branching | Depth First       | 31
+(20, 10, 3) | Pseudocost Branching | Best First        | 21
+(20, 10, 3) | Pseudocost Branching | Best Estimate     | 25
+(30, 20, 4) | Most Fraction        | Depth First       | 95
+(30, 20, 4) | Most Fraction        | Best First        | 95
+(30, 20, 4) | Most Fraction        | Best Estimate     | 95
+(30, 20, 4) | Fixed Branching      | Depth First       | 145
+(30, 20, 4) | Fixed Branching      | Best First        | 145
+(30, 20, 4) | Fixed Branching      | Best Estimate     | 145
+(30, 20, 4) | Pseudocost Branching | Depth First       | 69
+(30, 20, 4) | Pseudocost Branching | Best First        | 77
+(30, 20, 4) | Pseudocost Branching | Best Estimate     | 83
+(30, 20, 5) | Most Fraction        | Depth First       | 181
+(30, 20, 5) | Most Fraction        | Best First        | 181
+(30, 20, 5) | Most Fraction        | Best Estimate     | 181
+(30, 20, 5) | Fixed Branching      | Depth First       | 159
+(30, 20, 5) | Fixed Branching      | Best First        | 159
+(30, 20, 5) | Fixed Branching      | Best Estimate     | 159
+(30, 20, 5) | Pseudocost Branching | Depth First       | 113
+(30, 20, 5) | Pseudocost Branching | Best First        | 111
+(30, 20, 5) | Pseudocost Branching | Best Estimate     | 113
+(40, 20, 6) | Most Fraction        | Depth First       | 323
+(40, 20, 6) | Most Fraction        | Best First        | 181
+(40, 20, 6) | Most Fraction        | Best Estimate     | 209
+(40, 20, 6) | Fixed Branching      | Depth First       | 949
+(40, 20, 6) | Fixed Branching      | Best First        | 525
+(40, 20, 6) | Fixed Branching      | Best Estimate     | 562
+(40, 20, 6) | Pseudocost Branching | Depth First       | 177
+(40, 20, 6) | Pseudocost Branching | Best First        | 127
+(40, 20, 6) | Pseudocost Branching | Best Estimate     | 151
+(40, 20, 7) | Most Fraction        | Depth First       | 145
+(40, 20, 7) | Most Fraction        | Best First        | 71
+(40, 20, 7) | Most Fraction        | Best Estimate     | 74
+(40, 20, 7) | Fixed Branching      | Depth First       | 111
+(40, 20, 7) | Fixed Branching      | Best First        | 81
+(40, 20, 7) | Fixed Branching      | Best Estimate     | 81
+(40, 20, 7) | Pseudocost Branching | Depth First       | 81
+(40, 20, 7) | Pseudocost Branching | Best First        | 55
+(40, 20, 7) | Pseudocost Branching | Best Estimate     | 51
+(40, 30, 8) | Most Fraction        | Depth First       | 691
+(40, 30, 8) | Most Fraction        | Best First        | 411
+(40, 30, 8) | Most Fraction        | Best Estimate     | 424
+(40, 30, 8) | Fixed Branching      | Depth First       | 1083
+(40, 30, 8) | Fixed Branching      | Best First        | 393
+(40, 30, 8) | Fixed Branching      | Best Estimate     | 393
+(40, 30, 8) | Pseudocost Branching | Depth First       | 395
+(40, 30, 8) | Pseudocost Branching | Best First        | 167
+(40, 30, 8) | Pseudocost Branching | Best Estimate     | 253
+
 '''
 
 from grumpy import BBTree
