850a89cbd11ae13d3dd65cbb17cd87a76ce77822
Aykut Bulut <aykut@lehigh.edu>
2016-06-13 14:40:20 -0400

    __main__.py file added. Contents of this file is executed when command 'python -m coinor.grumpy' is called. Contents of this file is similar to run.py in examples directory. 'python -m coinor.grumpy' will read BAK format input from standart input and generate graphs and display them in pygame window.

---
+++ src/grumpy/__main__.py
@@ -0,0 +1,22 @@
+from coinor.grumpy import BBTree
+import sys
+import StringIO
+
+try:
+    from PIL import Image as PIL_Image
+except ImportError:
+    PIL_INSTALLED = False
+else:
+    PIL_INSTALLED = True
+
+if __name__ == '__main__':
+    bt = BBTree()
+    bt.set_display_mode('pygame')
+    line_number = 0
+    for line in sys.stdin:
+        bt.ProcessLine(line)
+        print 'line', line_number, 'processed.'
+        line_number = line_number+1
+        if line_number%10 != 0:
+            continue
+        bt.display_all()
