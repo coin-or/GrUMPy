fb54a2df528129131e650f1ffe2031cf34823660
Ted Ralphs <ted@lehigh.edu>
2016-08-10 23:16:04 +0200

    Updating the exaple script for generating tree images

--- src/grumpy/examples/run.py
+++ src/grumpy/examples/run.py
@@ -1,43 +1,30 @@
 from coinor.grumpy import BBTree
 import sys
 import StringIO
+from PIL import Image as PIL_Image
 
-try:
-    from PIL import Image as PIL_Image
-except ImportError:
-    PIL_INSTALLED = False
-else:
-    PIL_INSTALLED = True
+bt = BBTree()
+bt.set_display_mode('pygame')
+line_number = 0
+file_ = open('p0201_GLPK.in', 'r')
+#for line in sys.stdin:
+for line in file_:
+    bt.ProcessLine(line)
+    #To print out snapshots of the three
+    if line_number%100000 != 0:
+        continue
+    imagefile = open('tree-'+str(line_number)+'.png','w')
+    imagefile.write(bt.GenerateTreeImage())
+    imagefile.close()
 
-if __name__ == '__main__':
-    bt = BBTree()
-    bt.set_display_mode('pygame')
-    line_number = 0
-#    file_ = open('air04.bak', 'r')
-    for line in sys.stdin:
-#    for line in file_:
-        bt.ProcessLine(line)
-        print 'line', line_number, 'processed.'
-        line_number = line_number+1
-        if line_number%10000 != 0:
-            continue
-#        imagefile = open('tree-'+str(line_number)+'.png','w')
-#        imagefile.write(bt.GenerateTreeImage())
-#        imagefile.close()
-        if bt.root is not None:
-#            gnuplot_image = bt.GenerateHistogram()
-            gnuplot_image = StringIO.StringIO(bt.GenerateTreeImage())
-#            gnuplot_image = bt.GenerateScatterplot()
-#            gnuplot_image = bt.GenerateIncumbentPath()
-#            gnuplot_image = bt.GenerateForecastImages()
-            if gnuplot_image is not None and PIL_INSTALLED:
-                im = PIL_Image.open(gnuplot_image)
-                im.show()
-#            bt.display_all()
+#gnuplot_image = bt.GenerateHistogram()
+gnuplot_image = StringIO.StringIO(bt.GenerateTreeImage())
+#gnuplot_image = bt.GenerateScatterplot()
+#gnuplot_image = bt.GenerateIncumbentPath()
+#gnuplot_image = bt.GenerateForecastImages()
+im = PIL_Image.open(gnuplot_image)
+im.show()
+#bt.display_all()
 
-    gnuplot_image = StringIO.StringIO(bt.GenerateTreeImage())
-    if gnuplot_image is not None and PIL_INSTALLED:
-        im = PIL_Image.open(gnuplot_image)
-        im.show()
-#   bt.display_all()
-#    raw_input("Press a key to exit")
+bt.set_display_mode('xdot')
+bt.display()
