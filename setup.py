369eef760a30c32b4738347ba0509bcd732c0d0f
Ted Ralphs <ted@lehigh.edu>
2020-04-13 15:33:59 -0400

    Automatically finding packages and updating version number

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-import setuptools
+from setuptools import setup, find_packages
 
 setup(name='coinor.grumpy',
-      version='0.9.4',
+      version='0.95.0',
       description='Graphics for Understanding Mathematical Programming (GrUMPy)',
       long_description='''GrUMPy is a class for visualizing various algorithm used in solving discrete optimization problem. It has a class for dynamically generating and visualizing branch-and-bound trees that is derived from the GiMPy graph class. Using the branch-and-bound class, a user can visualize the branch-and-bound process in a number of different ways either by building the tree dynamically through direct calls to Python from the solver or by piping the output of an instrumented solver to GrUMPy for parsing. The branch-and-bound class also includes a pure Python implementation of branch and bound that is targeted at educational use.
 
@@ -23,9 +22,9 @@ https://tkralphs.github.io/GrUMPy
       license='Eclipse Public License',
       url='https://github.com/tkralphs/GrUMPy/',
       namespace_packages=['coinor'],
-      packages=['coinor.grumpy','coinor'],
+      packages=[pkg.replace('src','coinor') for pkg in find_packages()],
       package_data={'':['*.vbc']},
       include_package_data=True,
       package_dir = {'coinor': 'src'},
-      install_requires=['coinor.gimpy>=2.0.0', 'pulp']
+      install_requires=['coinor.gimpy>=2.1.0', 'pulp']
      )
